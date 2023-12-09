13cbd84ac4b95650d6b9c8b02035a4f5cd7f6b6d
Julio Siqueira <152822615+siqueirago@users.noreply.github.com>
2023-12-09 16:51:47 -0300

    Ajuste no código para resolver o desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,41 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-class Usuario
+//enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+//Criando classe Aluno
+class Aluno(
+    val nome: String ,
+    val Email: String ,
+    val idade: Int
+)
+// Criando a classe conteudo
+data class ConteudoEducacional(
+    var titulo: String, 
+    val duracaoHoras: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class Formacao(
+    val nome: String,
+    var nivel: String,
+    var conteudosEducacionais: List<ConteudoEducacional>) {
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    
+    fun matricular(aluno: Aluno) {
+        println("O aluno ${aluno.nome} foi matriculado no Curso:$nome - $nivel")
     }
 }
-
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //Criando conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 20)
+    val conteudo2 = ConteudoEducacional("Desenvolvendo Android com  Kotlin", 50)
+    // Criando a Formação
+    val formacaoKotlin = Formacao("Formação Kotlim","Intermediário",listOf(conteudo1, conteudo2))
+    
+    //Criando Alunos
+    val aluno = Aluno("Júlio", "julio@email.com", 45)
+    
+    //matriculando aluno na formação de Kotlin
+    formacaoKotlin.matricular(aluno)
+    
 }
